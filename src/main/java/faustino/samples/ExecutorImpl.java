bba70fa0f22f7cb3d46c38a36d5daeb9d5d7fb45
faustino <dinatalefausto@gmail.com>
2019-05-26 22:58:27 +0200

    log

--- src/main/java/faustino/samples/ExecutorImpl.java
+++ src/main/java/faustino/samples/ExecutorImpl.java
@@ -1,14 +1,17 @@
 package faustino.samples;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.function.BiConsumer;
 
 public class ExecutorImpl implements BiConsumer<String, Runnable> {
-
+    private final Logger log = LoggerFactory.getLogger(ExecutorImpl.class);
 
     @Override
     public void accept(String s, Runnable runnable) {
-        System.out.println("executor starting command " + s);
+        log.info("executor starting command {}", s);
         runnable.run();
-        System.out.println("executor executed command " + s);
+        log.info("executor executed command {}", s);
     }
 }
